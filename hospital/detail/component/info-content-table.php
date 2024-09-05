16534579c54e87908ed7cfb88d1d287ed811b338
Tien Dat <datphan.2floor@gmail.com>
2024-09-05 14:11:42 +0700

    update center alg

--- hospital/detail/component/info-content-table.php
+++ hospital/detail/component/info-content-table.php
@@ -8,27 +8,27 @@ if (!isset($infoHospital)) {
     <table class="table table-info">
         <tbody>
         <tr>
-            <th>医療機関名</th>
+            <th class="alg-center">医療機関名</th>
             <td><?php echo $infoHospital['name'] ?? '' ?></td>
         </tr>
         <tr>
-            <th>住所</th>
+            <th class="alg-center">住所</th>
             <td><?php echo $infoHospital['tel'] ?? '' ?></td>
         </tr>
         <tr>
-            <th>代表電話番号</th>
+            <th class="alg-center">代表電話番号</th>
             <td><?php echo $infoHospital['address'] ?? '' ?></td>
         </tr>
         <tr>
-            <th>公式HP</th>
+            <th class="alg-center">公式HP</th>
             <td><a target="_blank" href="<?php echo $infoHospital['hpUrl'] ?? '#' ?>"><?php echo $infoHospital['hpUrl'] ?? '' ?></a></td>
         </tr>
         <tr>
-            <th>がん相談支援センターURL</th>
+            <th class="alg-center">がん相談支援センターURL</th>
             <td><a target="_blank" href="<?php echo $infoHospital['supportUrl'] ?? '#' ?>"><?php echo $infoHospital['supportUrl'] ?? '' ?></a></td>
         </tr>
         <tr>
-            <th>特別室</th>
+            <th class="alg-center">特別室</th>
             <td><a target="_blank" href="<?php echo $infoHospital['specialClinicUrl'] ?? '#' ?>"><?php echo $infoHospital['specialClinicUrl'] ?? '' ?></a></td>
         </tr>
         </tbody>
--- hospital/detail/component/treatment-content-table.php
+++ hospital/detail/component/treatment-content-table.php
@@ -8,15 +8,15 @@ if (!isset($infoTreatment)) {
     <table class="table table-info table-treatment">
         <tbody>
         <tr>
-            <th>がん診療拠点区分</th>
+            <th class="alg-center">がん診療拠点区分</th>
             <td><?php echo $infoTreatment['hospitalType'] ?? '' ?></td>
         </tr>
         <tr>
-            <th>がんゲノム病院区分</th>
+            <th class="alg-center">がんゲノム病院区分</th>
             <td><?php echo $infoTreatment['hospitalGen'] ?? '' ?></td>
         </tr>
         <tr>
-            <th>集学的治療体制の状況</th>
+            <th class="alg-center">集学的治療体制の状況</th>
             <td>
                 <?php
                     echo $infoTreatment['multiTreatment'] ? '<span class="badge bg-secondary">あり</span>' : '';
@@ -24,7 +24,7 @@ if (!isset($infoTreatment)) {
             </td>
         </tr>
         <tr>
-            <th>名医の在籍状況</th>
+            <th class="alg-center">名医の在籍状況</th>
             <td>
                 <?php
                     echo $infoTreatment['famousDoctor'] ? '<span class="badge bg-secondary">あり</span>' : '<span class="badge bg-warning">なし</span>';
@@ -32,7 +32,7 @@ if (!isset($infoTreatment)) {
             </td>
         </tr>
         <tr>
-            <th>先進医療の提供状況</th>
+            <th class="alg-center">先進医療の提供状況</th>
             <td>
                 <?php
                 echo $infoTreatment['hasAdvancedMedical'] ? '<span class="badge bg-secondary">あり</span>' : '<span class="badge bg-warning">なし</span>';
@@ -42,7 +42,7 @@ if (!isset($infoTreatment)) {
             </td>
         </tr>
         <tr>
-            <th>特別な治療の提供状況</th>
+            <th class="alg-center">特別な治療の提供状況</th>
             <td>
                 <?php echo $infoTreatment['treatment'] ? nl2br(e($infoTreatment['treatment'])) : '<span class="badge bg-warning">なし</span>' ?>
             </td>
